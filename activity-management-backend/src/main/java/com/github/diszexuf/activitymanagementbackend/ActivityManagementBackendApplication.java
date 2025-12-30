8009b007ed5f22a241585de668de04ed4d9f0bb0
diszexuf <diszexuf@yandex.ru>
2025-12-30 16:05:45 +0500

    init commit

---
+++ activity-management-backend/src/main/java/com/github/diszexuf/activitymanagementbackend/ActivityManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.diszexuf.activitymanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityManagementBackendApplication.class, args);
+    }
+
+}
